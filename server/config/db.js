84bd0dfcfdcfc3fd884eb62e427902c08e1c2efe
rmmitc <riley.mitchell@student.unimelb.edu.au>
2024-08-11 15:02:21 +1000

    changed config files

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const uri = process.env.DATABASE_URI;
-const options = {
+
+// Depricated
+/*const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-};
+};*/
 
-module.exports = {uri, options};
+module.exports = {uri, /*options*/};
