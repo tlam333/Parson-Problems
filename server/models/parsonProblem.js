8dcdf073ba77b97a74e331ba13cc4da0e9f64b76
Riz-Mitchell <rileymitch117@gmail.com>
2024-09-02 13:16:33 +1000

    reformatted the file

--- server/models/parsonProblem.js
+++ server/models/parsonProblem.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PPSchema = new Schema({
-    prompt: { type: String, required: true }, // Description of the problem
-    correct_code: { type: [String], required: true }, // Array of strings for the correct code lines
-    scrambled_code: { type: [String], required: true }, // Array of strings for the scrambled code lines
-    feedback: { type: String, default:"" },
+    prompt: {
+        type: String,
+        required: true
+    }, // Description of the problem
+    correct_code: {
+        type: [String],
+        required: true 
+    }, // Array of strings for the correct code lines
+    scrambled_code: {
+        type: [String], required: true
+    }, // Array of strings for the scrambled code lines
+    feedback: {
+        type: String, default:""
+    },
 }, {
     timestamps: true,  // add createdAt and updatedAt timestamps
 });
