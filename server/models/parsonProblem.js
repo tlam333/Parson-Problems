7a78bc8a179aea72dddb0b07f09811b3f7081f51
Riz-Mitchell <rileymitch117@gmail.com>
2024-09-02 14:06:25 +1000

    editing database schema

--- server/models/parsonProblem.js
+++ server/models/parsonProblem.js
@@ -2,25 +2,35 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const PPSchema = new Schema({
+const parsonProblemSchema = new Schema({
+    userOwner: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    ipAddress: {
+        type: String,
+        required: true 
+    },
     prompt: {
         type: String,
         required: true
-    }, // Description of the problem
-    correct_code: {
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
+    blocks: {
         type: [String],
         required: true 
     }, // Array of strings for the correct code lines
-    scrambled_code: {
-        type: [String], required: true
-    }, // Array of strings for the scrambled code lines
-    feedback: {
-        type: String, default:""
-    },
+    solution: {
+        type: [String],
+        required: true
+    } // Array of strings for the scrambled code lines
 }, {
     timestamps: true,  // add createdAt and updatedAt timestamps
 });
 
-const ParsonProblem = mongoose.model('ParsonProblem', PPSchema);
+const ParsonProblem = mongoose.model('ParsonProblem', parsonProblemSchema);
 
 module.exports = ParsonProblem;
