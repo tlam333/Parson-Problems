9095d7498fafeea0575f2487d5ac69b73dc794dc
JiayangTan <jiayangt@student.unimelb.edu.au>
2024-09-08 20:48:19 +1000

    fixed left button of nav bar

--- client/app/src/components/NavMenu.jsx
+++ client/app/src/components/NavMenu.jsx
@@ -6,7 +6,9 @@ const NavMenu = () => {
     <nav className="flex justify-between items-center bg-black p-4">
       {/* Left Button - Orange Button */}
       <div className="flex items-center">
-        <button className="btn bg-orange-500 text-white p-6 rounded-xl"></button>
+        <Link to = "/Home">
+          <button className="btn bg-orange-500 text-white p-6 rounded-xl"></button>
+        </Link>
       </div>
       
       {/* Right Menu - Dropdown */}
