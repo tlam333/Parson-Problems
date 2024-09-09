219ab5473732fca336ef786faa2689d0200a73e7
tlam333 <tsanh@student.unimelb.edu.au>
2024-09-09 13:03:43 +1000

    Styled workpage, and slight changes to savedproblems

--- client/app/src/pages/SavedProblems.jsx
+++ client/app/src/pages/SavedProblems.jsx
@@ -1,5 +1,6 @@
 // import '../styles/savedProblems.css';
 import { createElement } from 'react';
+import NavMenu from "../components/NavMenu";
 
 function FetchProblems(){
     let problems;
@@ -80,43 +81,42 @@ const SavedProblems = () => {
                     ]
 
     return (
+
         <div className='h-lvh bg-black'>
-                <div className="w-4/6 m-auto h-1/6 p-10 bg-black">
-                    <p className="text-lg text-white font-bold block">Saved Problems</p>
-                    
-                </div>
 
+    <NavMenu />
+            
+    <h1 className="text-5xl font-bold text-white text-center mt-4">
+        Saved <span className="text-orange-500">Problems</span>
+    </h1>
+    <br />            
+    <div className='overflow-y-auto border-orange-500 mt-10 border-2 w-5/6 h-4/6 bg-black m-auto flex flex-col'>
+        <table className='table-auto h-auto w-full m-auto'>
+            <thead className='sticky top-0'>
+                <tr className='text-white'>
+                    <td className='p-3 bg-orange-500'>Mark As Complete</td>
+                    <td className='p-3 bg-orange-500'>Problem</td>
+                    <td className='p-3 bg-orange-500'>Category</td>
+                    <td className='p-3 bg-orange-500'>Status</td>
+                </tr>
+            </thead>
 
-                
+            <tbody className="bg-black h-auto">
+                {problems.map((item, index) => (
+                    <tr key={index} className='hover:bg-slate-900 text-white'>
+                        <td className='p-3 text-sm border-b-orange-500 border-b-2'><input type='checkbox'></input></td>
+                        <td className='p-3 text-sm border-b-orange-500 border-b-2 font-bold hover:text-orange-500'><button className="underline" href="/">{item.problemName}</button></td>
+                        <td className='p-3 text-sm border-b-orange-500 border-b-2'>{item.problemCategory}</td>
+                        <td className='p-3 text-sm border-b-orange-500 border-b-2'>
+                            <span className={`rounded-lg text-white p-2 ${item.solvedStatus === 'Complete' ? 'bg-green-300' : 'bg-red-300'}`}>{item.solvedStatus}</span>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+</div>
 
-                <div className='overflow-y-auto  border-orange-500 mt-10 border-2 w-5/6 h-4/6 bg-black m-auto flex flex-col'>
-                    <table className='table-auto h-auto w-full m-auto'>
-                        <thead className='sticky top-0'>
-                            <tr className='text-white'>
-                                <td className='p-3 bg-orange-500'>Mark As Complete</td>
-                                <td className='p-3 bg-orange-500'>Problem</td>
-                                <td className='p-3 bg-orange-500'>Category</td>
-                                <td className='p-3 bg-orange-500'>Status</td>
-                            </tr>
-                        </thead>
-
-                        <tbody className="bg-black h-auto">
-                            {problems.map((item, index) => (
-                                <tr className='hover:bg-slate-900 text-white'>
-                                    <td className='p-3 text-sm border-b-orange-500 border-b-2'><input type='checkbox'></input></td>
-                                    <td className='p-3 text-sm border-b-orange-500 border-b-2 font-bold hover:text-orange-500'><button className="underline" href="/">{item.problemName}</button></td>
-                                    <td className='p-3 text-sm border-b-orange-500 border-b-2'>{item.problemCategory}</td>
-                                    <td className='p-3 text-sm border-b-orange-500 border-b-2'>
-                                        <span className={`rounded-lg text-white p-2 ${item.solvedStatus == 'Complete' ? 'bg-green-300' : 'bg-red-300'}`}>{item.solvedStatus}</span>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
-                
-        </div>
         
 
     )
