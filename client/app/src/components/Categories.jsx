ed7a633d301cc243d8eb543ac3c4399247095dba
JiayangTan <jiayangt@student.unimelb.edu.au>
2024-10-12 20:13:51 +1100

    Edited the categories page and added an extra subtopic

--- client/app/src/components/Categories.jsx
+++ client/app/src/components/Categories.jsx
@@ -8,27 +8,20 @@ const Categories = ({ categories }) => {
     return (
         <div className="bg-black flex flex-wrap gap-4 justify-center">
             {categories.map((category, index) => (
-                <div key={index} className="card glass w-96">
+                <div key={index} className="card glass w-96 text-white font-bold">
                     <figure>
                         <img src={category.imgSrc} alt={category.title} />
                     </figure>
                     <div className="card-body">
                         <h2 className="card-title">{category.title}</h2>
-                        <Link to ="/WorkPage"
-                            state = {{topic : category.title, theme : category.subtopic}}>
-                        <p>{category.description}</p>
-                        </Link>
-                        <Link to ="/WorkPage"
-                            state = {{topic : category.title, theme : category.subtopic}}
-                        >
-                        <p>{category.subtopic}</p>
-                        </Link>
-                        <div className="card-actions justify-end">
-                        <Link to ="/WorkPage"
-                            state = {{topic : category.title, theme : category.subtopic}}>
-                                <button className="bg-orange-500 hover:bg-orange-600 text-black font-bold py-2 px-4 rounded">Learn now!</button>
-                            </Link>
-                        </div>
+                        <ul className="menu bg-orange-500 text-black font-bold py-2 px-4 rounded">
+                        <Link to = "/WorkPage"
+                            state = {{topic : category.title, theme : category.subtopic1}}><li><a className="hover:bg-orange-600">{category.subtopic1}</a></li></Link>
+                        <Link to = "/WorkPage"
+                            state = {{topic : category.title, theme : category.subtopic2}}><li><a className="hover:bg-orange-600">{category.subtopic2}</a></li></Link>
+                        <Link to = "/WorkPage"
+                            state = {{topic : category.title, theme : category.subtopic3}}><li><a className="hover:bg-orange-600">{category.subtopic3}</a></li></Link>
+                        </ul>
                     </div>
                 </div>
             ))}
