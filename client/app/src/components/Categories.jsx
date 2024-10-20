08148c2948e8123502adc8209a0c92493596f739
Riz-Mitchell <rileymitch117@gmail.com>
2024-10-20 14:57:35 +1100

    Updated categories to show new workpage

--- client/app/src/components/Categories.jsx
+++ client/app/src/components/Categories.jsx
@@ -15,11 +15,11 @@ const Categories = ({ categories }) => {
                     <div className="card-body">
                         <h2 className="card-title">{category.title}</h2>
                         <ul className="menu bg-orange-500 text-black font-bold py-2 px-4 rounded">
-                        <Link to = "/WorkPage"
+                        <Link to = "/WorkPage2"
                             state = {{topic : category.title, theme : category.subtopic1}}><li><a className="hover:bg-orange-600">{category.subtopic1}</a></li></Link>
-                        <Link to = "/WorkPage"
+                        <Link to = "/WorkPage2"
                             state = {{topic : category.title, theme : category.subtopic2}}><li><a className="hover:bg-orange-600">{category.subtopic2}</a></li></Link>
-                        <Link to = "/WorkPage"
+                        <Link to = "/WorkPage2"
                             state = {{topic : category.title, theme : category.subtopic3}}><li><a className="hover:bg-orange-600">{category.subtopic3}</a></li></Link>
                         </ul>
                     </div>
